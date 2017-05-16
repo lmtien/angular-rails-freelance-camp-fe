9ff7e074e2401b8306c731e569729019fa5c7a43
Tidus Le <tiduslmt@gmail.com>
2017-05-16 16:27:04 +0000

    add documents

---
+++ src/app/documents/documents.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'documents',
+    templateUrl: 'documents.component.html'
+})
+
+export class DocumentsComponent {}
