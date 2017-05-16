c62c3b19fab0c06e8c6947f0602d07918b209989
Tidus Le <tiduslmt@gmail.com>
2017-05-16 16:13:56 +0000

    add views for components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'app',
-    template: `
-    
-    <h1>Navigation</h1>
-    
-    <router-outlet></router-outlet>
-    `
+    templateUrl: 'app.component.html'
 })
 
 export class AppComponent {
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'homepage',
-    template: '<h1>Homepage</h1>'    
+    templateUrl: 'homepage.component.html'
 })
 
 export class HomepageComponent {}
