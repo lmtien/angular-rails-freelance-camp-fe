6cb12c69ac9d158ed1bb4ee54c31f6d669f933bb
Tidus Le <tiduslmt@gmail.com>
2017-05-17 06:31:36 +0000

    add Proposal Show

---
+++ src/app/proposal/proposal-show.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Proposal } from './proposal';
+
+@Component({
+    moduleId: module.id,
+    selector: 'proposal-show',
+    templateUrl: 'proposal-show.component.html'
+})
+
+export class ProposalShowComponent implements OnInit {
+    id: number;
+    routeId: any;
+    
+    constructor(
+        private route: ActivatedRoute
+    ) {}
+    
+    ngOnInit(): void {
+        this.routeId = this.route.params.subscribe(
+            params => {
+                this.id = +params['id'];
+            }
+        )
+    }
+}
