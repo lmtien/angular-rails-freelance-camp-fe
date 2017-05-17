81a9a67effd8ae866d797ab21c783fe702638dd7
Tidus Le <tiduslmt@gmail.com>
2017-05-17 08:35:10 +0000

    organizing styles

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -4,7 +4,8 @@ import { Document } from './document';
 @Component({
     moduleId: module.id,
     selector: 'documents',
-    templateUrl: 'documents.component.html'
+    templateUrl: 'documents.component.html',
+    styleUrls: ['documents.component.css']
 })
 
 export class DocumentsComponent {
@@ -16,21 +17,21 @@ export class DocumentsComponent {
             description: 'abcxyz',
             file_url: 'google.com',
             updated_at: '17/05/2017',
-            image_url: 'google.com'
+            image_url: 'http://www.publicdomainpictures.net/pictures/220000/t2/los-gehts.jpg'
         },
         {
             title: 'My second doc',
             description: 'abcxyz',
             file_url: 'google.com',
             updated_at: '17/05/2017',
-            image_url: 'google.com'
+            image_url: 'https://static.pexels.com/photos/156/man-person-apple-iphone.jpg'
         },
         {
             title: 'My last doc',
             description: 'abcxyz',
             file_url: 'google.com',
             updated_at: '17/05/2017',
-            image_url: 'google.com'
+            image_url: 'https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_960_720.jpg'
         },
     ]
 }
